bcdd98fec59558a82406592681ec161ada8c224f
abooraamoraa-code <abooraamoraa@gmail.com>
2026-09-17 10:52:00 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+﻿/**
+ * إعدادات الاتصال السحابي وحماية لوحة الإدارة الفاخرة لـ Maha Zone
+ */
+const StoreConfig = {
+    apiKey: "$2a$10$Hj8N/vpuugwh9k/pupGakulkBIE4pq3W09JTAUiUKbtJfiWBdpJHS",
+    binId: "6aaa882affd5d160530df70d",
+    baseUrl: "https://api.jsonbin.io/v3/b",
+    
+    // كلمة المرور الجديدة للوحة التحكم
+    adminPassword: "HAZEM2012",
+    
+    // رابط صفحة الفيسبوك للتواصل المباشر
+    facebookUrl: "https://www.facebook.com" 
+};
