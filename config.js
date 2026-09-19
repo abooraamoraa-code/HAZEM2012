891f7ad7ea64d89852ff10c959bcb2ab9b43441c
abooraamoraa-code <abooraamoraa@gmail.com>
2026-09-19 10:57:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
-/**
- * إعدادات الاتصال السحابي وحماية لوحة الإدارة الفاخرة لـ Maha Zone
- */
 const StoreConfig = {
     apiKey: "$2a$10$Hj8N/vpuugwh9k/pupGakulkBIE4pq3W09JTAUiUKbtJfiWBdpJHS",
     binId: "6aaa882affd5d160530df70d",
     baseUrl: "https://api.jsonbin.io/v3/b",
-    
-    // كلمة المرور الجديدة للوحة التحكم
     adminPassword: "HAZEM2012",
     
-    // رابط التواصل المباشر (يمكنك استبداله برابط مسنجر أو بحث مباشر مخصص لمها زون)
-    facebookUrl: "https://www.google.com/search?q=Maha+Zone" 
+    // رابط نتائج البحث المباشر عن مها زون
+    facebookUrl: "https://www.google.com/search?q=Maha+Zone+%D9%85%D9%87%D8%A7+%D8%B2%D9%88%D9%86" 
 };
