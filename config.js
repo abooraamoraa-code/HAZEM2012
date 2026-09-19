6bc24b5c7cfcfa9f51696a36d9a9643358b2ba47
abooraamoraa-code <abooraamoraa@gmail.com>
2026-09-19 10:58:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const StoreConfig = {
     baseUrl: "https://api.jsonbin.io/v3/b",
     adminPassword: "HAZEM2012",
     
-    // رابط نتائج البحث المباشر عن مها زون
-    facebookUrl: "https://www.google.com/search?q=Maha+Zone+%D9%85%D9%87%D8%A7+%D8%B2%D9%88%D9%86" 
+    // ضع رابط صفحة مسنجر الخاصة بك هنا (استبدل اسم الصفحة بمعرف صفحتك الفعلي)
+    facebookUrl: "https://m.me/MahaZonePage" 
 };
